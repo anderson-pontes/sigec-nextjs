71d001d2dbdb4db63161423a4681a772b421695f
Anderson Pontes <andersonpontes.dev@gmail.com>
2023-12-28 14:51:30 -0300

    update TabItem

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,13 +1,14 @@
 'use client'
-
 import * as Tabs from '@radix-ui/react-tabs';
+import { TabItem } from './TabItem';
 export function SettingsTabs() {
   return (
     <Tabs.Root >
       <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <Tabs.Trigger value="detalhes" className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-red-600">
-          <span>Detalhes</span>
-        </Tabs.Trigger>      
+             <TabItem value="detalhes" title="Detalhes" isSelected />
+             <TabItem value="perfil" title="Perfil" />
+             <TabItem value="setor" title="Setor" />
+             <TabItem value="password" title="Password" />
       </Tabs.List>    
     </Tabs.Root>  
   )
